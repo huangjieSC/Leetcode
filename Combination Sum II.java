3637bf8d9e57b341a02fd81a1a851de23a88095e
Gia Huang <huangjiebupt@gmail.com>
2015-05-03 22:19:06 -0400

    Create Combination Sum II.java

---
+++ Combination Sum II.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> combinationSum2(int[] candidates, int target) {
+       
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if(candidates==null || candidates.length==0) return result;
+        
+        Arrays.sort(candidates);
+        
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        dfs(candidates, target, 0, item, result);
+        return result;
+    }
+    
+    public void dfs(int[] candidates, int target, int start, ArrayList<Integer> item, ArrayList<ArrayList<Integer>> result){
+        if(target<0) return;
+        
+        if(target==0){
+            if(!result.contains(item))
+                result.add(new ArrayList<Integer>(item));
+            return;
+        }
+        
+        for(int i=start; i<candidates.length; i++){
+            if(candidates[i]<=target){
+                item.add(candidates[i]);
+                dfs(candidates,target-candidates[i],i+1,item,result);
+                item.remove(item.size()-1);
+            }
+            else
+                return;
+        }
+    }
+}
