547e55818829a4e054378a3c21b622dcc801c616
Gia Huang <huangjiebupt@gmail.com>
2015-05-05 19:58:52 -0400

    Create 27-Remove Element.java

---
+++ 27-Remove Element.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums==null || nums.length==0) return 0;
+        
+        int index=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==val) continue;
+            nums[index] = nums[i];
+            index++;
+        }
+        return index;
+    }
+}
