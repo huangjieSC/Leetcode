8c12cb06333a6c410da784d572fa6ea77072f51d
Gia Huang <huangjiebupt@gmail.com>
2015-05-05 19:23:20 -0400

    Update 101-Symmetric Tree.java

--- 101-Symmetric Tree.java
+++ 101-Symmetric Tree.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //Recursive solution
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null) return true;
@@ -20,5 +21,48 @@ public class Solution {
         
         if(left.val!=right.val) return false;
         return helper(left.left, right.right) && helper(left.right,right.left);
+    }
+}
+
+//Iterative solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+       if(root==null) return true;
+       
+       ArrayList<TreeNode> current = new ArrayList<TreeNode>();
+       current.add(root);
+       
+       while(current.size()!=0){
+           ArrayList<TreeNode> next = new ArrayList<TreeNode>();
+           int len = current.size();
+           int start = 0, end = len-1;
+           while(start<len && end>=0 && start<end){
+               int l=current.get(start)==null?-1:current.get(start).val;
+               int r=current.get(end)==null?-1:current.get(end).val;
+          
+               if(l!=r) return false;
+               start++;
+               end--;
+           }
+           for(TreeNode node: current){
+               if(node==null) continue;
+               if(node!=null){
+                   next.add(node.left);
+                   next.add(node.right);
+               }
+           }
+           current = next;
+       }
+       return true;
     }
 }
