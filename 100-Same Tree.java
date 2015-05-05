5852a932b221005b8a321d8448ef3652317c3c40
Gia Huang <huangjiebupt@gmail.com>
2015-05-05 18:30:25 -0400

    Update 100-Same Tree.java

--- 100-Same Tree.java
+++ 100-Same Tree.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ //Recursive solution
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null) return true;
@@ -16,3 +17,41 @@ public class Solution {
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
+
+//Iterative solution, use queue, idea is similar to BFS, level order comparison
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        Queue<TreeNode> left = new LinkedList<TreeNode>();
+        Queue<TreeNode> right = new LinkedList<TreeNode>();
+         
+        left.add(p);
+        right.add(q);
+         
+        while (!left.isEmpty() && !right.isEmpty()) {
+            TreeNode ln = left.poll();
+            TreeNode rn = right.poll();
+            
+            if (ln == null && rn == null) continue;
+            if (ln == null || rn == null) return false;
+            if (ln.val != rn.val) return false;
+             
+            left.add(ln.left); left.add(ln.right);
+            right.add(rn.left); right.add(rn.right);
+        }
+         
+        if(!left.isEmpty() || !right.isEmpty())  return false;
+        return true;
+    }
+}
+
