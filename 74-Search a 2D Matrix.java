1ed41313bfedbb5395d0de622161a6d2e8776258
Gia Huang <huangjiebupt@gmail.com>
2015-05-09 23:27:45 -0400

    Create 74-Search a 2D Matrix.java

---
+++ 74-Search a 2D Matrix.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null || matrix.length==0 || matrix[0].length==0) return false;
+        
+        int rowLen = matrix.length, colLen = matrix[0].length;
+        int i=0, j=colLen-1;
+        
+        while(i<rowLen && j>=0){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target) i++;
+            else j--;
+        }
+        return false;
+    }
+}
