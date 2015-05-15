296102088927dff13ac5e1241a2ff3111f4cca05
Gia Huang <huangjiebupt@gmail.com>
2015-05-15 19:30:05 -0400

    Create 78-Subsets.java

---
+++ 78-Subsets.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int[] nums) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        if(nums==null || nums.length==0) return list;
+        
+        Arrays.sort(nums);
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        
+        for(int i=0; i<=nums.length; i++){
+            combinations(nums,0, i, item, list);
+        }
+        return list;
+    }
+    
+    public void combinations(int[] nums, int start, int size, ArrayList<Integer> item, ArrayList<ArrayList<Integer>> list){
+        if(item.size()==size){
+            list.add(new ArrayList<Integer>(item));
+            return;
+        }
+        
+        for(int i=start; i<nums.length; i++){
+            item.add(nums[i]);
+            combinations(nums, i+1,size, item, list);
+            item.remove(item.size()-1);
+        }
+    }
+}
