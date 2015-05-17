f416ef187884bf54cf535bd372ddfb7c7be6f6c2
Gia Huang <huangjiebupt@gmail.com>
2015-05-17 15:50:15 -0400

    Create 37-Sudoku Solver.java

---
+++ 37-Sudoku Solver.java
@@ -0,0 +1,54 @@
+public class Solution {
+    /**
+  N皇后问题。就是一点点尝试着填数，不行的话就回溯，直到都填满就返回。 
+如果对一个格子尝试从0~9都不行，那么说明整个sudoku无解，返回false就好。
+对整个棋盘所有'.'都填完了，那么就可以返回true了。
+     * */
+    
+    public void solveSudoku(char[][] board) {
+        if (board==null||board.length==0)
+            return;
+        helper(board);
+    }
+    
+    private boolean helper(char[][] board){
+        for(int i=0; i<board.length; i++){
+            for (int j=0; j<board[0].length; j++){
+                if (board[i][j]=='.'){
+                    for (char num='1'; num<='9'; num++){//尝试
+                        if(isValid(board, i, j, num)){
+                            board[i][j]=num;
+                            
+                            if (helper(board))
+                                return true;
+                            else
+                                board[i][j]='.';//回退
+                        }
+                    }
+                    return false; // don't forget this
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int i, int j, char c){
+        // check column
+        for (int row=0; row<9; row++)
+            if (board[row][j] == c)
+                return false;
+        
+       // check row
+        for (int col=0; col<9; col++)
+            if (board[i][col]==c)
+                return false;
+      
+        // check block
+        for(int row=(i/3)*3; row<(i/3)*3+3; row++)
+            for (int col=(j/3)*3; col<(j/3)*3+3; col++)
+                if (board[row][col]==c)
+                    return false;
+                    
+        return true;
+    }
+}
